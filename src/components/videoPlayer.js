d53a935bd6c64a2ac054134acd1d5ae858184eda
d-kang <david.kang714@gmail.com>
2017-07-22 16:47:22 -0700

    Video Player Does Not Render A When None is Provided

--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -1,10 +1,14 @@
 angular.module('video-player')
   .controller('videoPlayerController', function() {
-
+    this.youtubeURL = (url) => {
+      console.log({url});
+      return url ? `https://www.youtube.com/embed/${url}` : '';
+    };
   })
   .component('videoPlayer', {
     bindings: {
       video: '<'
     },
+    controller: 'videoPlayerController',
     templateUrl: 'src/templates/videoPlayer.html'
   });
